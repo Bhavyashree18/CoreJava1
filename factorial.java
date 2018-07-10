67458310258cb2880953288fe085c77a606d5c34
Bhavyashree18 <41058455+Bhavyashree18@users.noreply.github.com>
2018-07-10 17:49:40 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,22 @@
+class factorail {
+
+    
+    public static int fact(int n)
+    {
+        if(n==0)
+        {
+            return 0;
+        }
+        else if(n==1)
+        {
+            return 1;
+        }
+        else
+            return n*fact(n-1);
+    }
+    public static void main(String[] args) {
+        
+System.out.println(fact(5));
+    }
+
+}
